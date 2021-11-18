84619a3571c9a5b93fd4d502913441db1d870775
Buksha Kirill <buksha1024@gmail.com>
2021-11-18 18:43:42 +0300

    Separated classes

--- Program.cs
+++ Program.cs
@@ -42,36 +42,4 @@ namespace Lab1_mag
             Console.WriteLine();
         }
     }
-
-    class TwoDimensionalVector
-    {
-        public double FirstDimension { get; private set; }
-
-        public double SecondDimension { get; private set; }
-
-        public double Length { get; private set; }
-
-        public TwoDimensionalVector(double firstDimension, double secondDimension)
-        {
-            FirstDimension = firstDimension;
-            SecondDimension = secondDimension;
-            Length = Math.Sqrt(Math.Pow(FirstDimension, 2.0) + Math.Pow(SecondDimension, 2.0));
-        }
-
-        public int CompareTo(TwoDimensionalVector other)
-        {
-            var diff = Length - other.Length;
-            return diff switch
-            {
-                > 0 => 1,
-                0 => 0,
-                _ => -1
-            };
-        }
-
-        public override string ToString()
-        {
-            return $"({FirstDimension}, {SecondDimension})";
-        }
-    }
 }
---
+++ TwoDimensionalVector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab1_mag
+{
+    class TwoDimensionalVector
+    {
+        public double FirstDimension { get; private set; }
+
+        public double SecondDimension { get; private set; }
+
+        public double Length { get; private set; }
+
+        public TwoDimensionalVector(double firstDimension, double secondDimension)
+        {
+            FirstDimension = firstDimension;
+            SecondDimension = secondDimension;
+            Length = Math.Sqrt(Math.Pow(FirstDimension, 2.0) + Math.Pow(SecondDimension, 2.0));
+        }
+
+        public int CompareTo(TwoDimensionalVector other)
+        {
+            var diff = Length - other.Length;
+            return diff switch
+            {
+                > 0 => 1,
+                0 => 0,
+                _ => -1
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"({FirstDimension}, {SecondDimension})";
+        }
+    }
+}
